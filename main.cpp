1b06bfabc48b83fcb83b0c63fa6d5ca6553d8c05
umidaboqijonova <u.boqijonova@newuu.uz>
2025-04-06 18:57:18 +0500

    .

--- main.cpp
+++ main.cpp
@@ -1,266 +1,302 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
-//problem 14 lab 6
-
-// void welcomeMessage() {
-//     cout<<"Welcome to the C++ programming world!";
-// }
-//
-//     int main() {
-//         welcomeMessage();
-//     }
-
-
-
-//class problem 1
-
-//const double PI=3.141592653589793;
-// double area(double r) {
-//     return 3.14 * r * r;
-// }
-// double circumference(double r) {
-//     return 2 * 3.14 * r;
-// }
-// double volume(double r) {
-//     return 4.0/3.0 * 3.14 * r*r*r;
-// }
-//
+//problem 1
 // int main() {
-//     double r;
-//     cin>>r;
-//     cout<<"area of circle: "<<area(r)<<endl;
-//     cout<<"circumference: "<<circumference(r)<<endl;
-//     cout<<"volume: "<<volume(r);
-// }
-
-//class problem 2
-
-// void incrementByValue(int a) {
-//     a += 1;
-// }
-// void incrementByReference(int &a) {
-//     a += 1;
-// }
-// int main() {
-//     int num;
-//     cin >> num;
-//     incrementByValue(num);
-//     cout<<"after call by value: "<<num<<endl;
-//     incrementByReference(num);
-//     cout<<"after call by reference: "<<num<<endl;
-// }
-
-//problem 1 lab 7
-
-// void menu() {
-//     cout<<"==== Restaurant Menu ====\n1. Pizza ($10)\n2. Burger ($8)\n3. Salad ($5)\n";
-// }
-// int cost(int price, int quantity) {
-//     return price * quantity;
-// }
-// int main() {
-//     int total_bill=0;
-//     char orderMore;
-//     int choice, price, quantity;
-//     do {
-//         menu();
-//         cout<<"Enter you choice (1-3): ";
-//         cin>>choice;
-//         switch(choice) {
-//             case 1:
-//                 price=10;
-//                 cout<<"Enter quantity for Pizza: ";
-//             cin>>quantity;
-//             break;
-//             case 2:
-//                 price=8;
-//                 cout<<"Enter quantity for Burger: ";
-//             cin>>quantity;
-//             break;
-//             case 3:
-//                 price=5;
-//                 cout<<"Enter quantity for Salad: ";
-//             cin>>quantity;
-//             break;
-//             default:
-//                 cout<<"Invalid choice, please try again";
-//             continue;
-//
+//     int n,m;
+//     int sum=0;
+//     cout<<"Enter the number of rows and columns of matrix:";
+//     cin>>n>>m;
+//     int arr[n][m];
+//     for (int i=0;i<n;i++) {
+//         for (int j=0;j<m;j++) {
+//             cin>>arr[i][j];
+//             sum += arr[i][j];
 //         }
-//         int bill = cost(price, quantity);
-//         total_bill+=bill;
-//             cout<<"Cost for this order: $"<<bill<<endl;
-//         cout<<"Running total: "<<total_bill<<endl;
-//         cout<<"Do you want to order more? (y/n):";
-//         cin>>orderMore;
-//     }while(orderMore=='y' || orderMore=='Y');
-//     cout<<"\nTotal cost is $"<<total_bill;
-//     return 0;
+//         cout<<endl;
+//     }
+//     cout<<"The sum is: "<<sum;
 // }
-
 
 //problem 2
-
-// double parkingFee(double hours) {
-//     double totalFee=0;
-//     if (hours>2) {
-//         if (hours<=5) {
-//             totalFee=(hours-2)*2;
-//         } else {
-//             totalFee=6+(hours-5)*5;
+// int main() {
+//     int n,m;
+//     cout<<"Enter the number of rows and columns of matrix:"; cin>>n>>m;
+//     int arr[n][m];
+//     cout<<"Enter the elements of matrix"<<endl;
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < m; j++) {
+//             cin>>arr [i][j];
 //         }
 //     }
-//     return (totalFee);
-// }
-// int main() {
-//     double hours, price;
-//     char choice;
-//     do {
-//         cout<<"Hours"; cin>>hours;
-//         price=parkingFee(hours);
-//         cout<<"Fee is $"<<price;
-//         cout<<"\nIs there any other user? (y/n)"; cin>>choice;
-//     }while (choice=='y' || choice=='Y');
+//     for (int i = 0; i < n; i++) {
+//         int max = arr[i][0];
+//         for (int j = 0; j < m; j++) {
+//             if (arr[i][j] > max) {
+//                 max = arr[i][j];
+//             }
+//         }
+//         cout<<"Maximum number in a row: "<< max<<endl;
+//     }
+//     cout<< endl;
 // }
 
 //problem 3
-//deposit is adding; withdrawal is removing; balance inquiry is current balance;
-// double balance =500;
-// void deposit(double amount) {
-//     balance += amount;
-// }
-// void withdrawal(double amount) {
-//     if (balance >= amount) {
-//         balance -= amount;
-//     }else {
-//         cout<<"Insufficient funds\n";
-//     }
-// }
 // int main() {
-//     char choice;
-//     do{
-//         double amount = 0;
-//         cout<<"=== Banking System ===\nChoose operation:\n1. Deposit\n2. Withdraw\n3.Check Balance\n4. Finish";
-//         cin>>choice;
-//         switch (choice) {
-//             case '1':
-//                 cout<<"Enter amount to deposit:"; cin>>amount;
-//                 deposit(amount);
-//             cout<<"Balance: $"<<balance<<endl;
-//             break;
-//             case '2':
-//                 cout<<"Enter amount to withdrawal:"; cin>>amount;
-//                 withdrawal(amount);
-//             cout<<"Balance: $"<<balance<<endl;
-//             break;
-//             case '3':
-//                 cout<<"Your Balance is $"<<balance<<endl;
-//             break;
-//             case '4':
-//                 cout<<"Exiting...";
-//                 break;
-//             default:
-//                 cout<<"Invalid Choice";
+//     int n,m;
+//     cout<<"Enter the number of rows and columns of matrix:"; cin>>n>>m;
+//     int arr[n][m];
+//     cout<<"Enter the elements:"<<endl;
+//     for (int i = 0; i < n; i++) { //rows
+//         for (int j = 0; j < m; j++) { //columns
+//             cin>>arr[i][j];
 //         }
-//     }while (choice!='4');
+//     }
+//     for (int j = 0; j < m; j++) {
+//         int sum = 0;
+//         for (int i = 0; i < n; i++) {
+//             sum += arr[i][j];
+//         }
+//         cout<<sum<<" ";
+//     }
+//     cout<<endl;
 // }
 
 //problem 4
-//
-// void movie() {
-//     cout<<"=== Movie Tickets ===\n1. Movie A: $8\n2. Movie B: $10\n3. Movie C: $12\n";
-// }
-// int cost(int price, int quantity){
-//         return price*quantity;
-//     }
 // int main() {
-//     int total_bill=0;
-//     char orderMore;
-//     int choice, price, quantity;
-//     do {
-//         movie();
-//         cout<<"Enter you choice (1-3): ";
-//         cin>>choice;
-//         switch(choice) {
-//             case 1:
-//                 price=8;
-//                 cout<<"Enter number of tickets: ";
-//             cin>>quantity;
-//             break;
-//             case 2:
-//                 price=10;
-//                 cout<<"Enter number of tickets: ";
-//             cin>>quantity;
-//             break;
-//             case 3:
-//                 price=12;
-//                 cout<<"Enter number of tickets: ";
-//             cin>>quantity;
-//             break;
-//             default:
-//                 cout<<"Invalid choice, please try again";
-//             continue;
-//
+//     int n,m;
+//     cout<<"Enter the size of square matrix:";
+//     cin>>n>>m;
+//     int arr[n][m];
+//     cout<<"Enter the elements: "<<endl;
+//     for (int i=0;i<n;i++) {
+//         for (int j=0;j<m;j++) {
+//             cin>>arr[i][j];
 //         }
-//         int bill = cost(price, quantity);
-//         total_bill+=bill;
-//             cout<<"Total cost: $"<<bill<<endl;
-//         cout<<"Running total: "<<total_bill<<endl;
-//         cout<<"Do you want to buy more? (y/n):";
-//         cin>>orderMore;
-//     }while(orderMore=='y' || orderMore=='Y');
-//     cout<<"\nTotal cost is $"<<total_bill;
-//     return 0;
+//     }
+//     int sum=0;
+//     for (int i=0;i<n;i++) {
+//         sum += arr[i][i];
+//     }
+//     cout<<sum<<endl;
 // }
 
 //problem 5
-//
-// void train() {
-//     cout<<"=== Available Trains ===\n1. Train X: $15\n2. Train Y: $20\n3. Train Z: $25\n4. Finish\n";
+// int main() {
+//     int n,m;
+//     cout<<"Enter the number of rows and columns of matrix: ";
+//     cin>>n>>m;
+//     int arr_org [n][m];
+//     int arr_new [m][n];
+//     cout<<"The elements: "<<endl;
+//     for (int i = 0; i < n; i++) { //rows
+//         for (int j = 0; j < m; j++) { //columns
+//             cin>>arr_org [i][j];
+//         }
+//     }
+//     for (int i = 0; i < n; i++) {
+//         for ( int j = 0; j < m; j++) {
+//             arr_new [j][i] = arr_org [i][j];
+//         }
+//     }
+//     for (int i = 0; i < m; i++) { //m is rows
+//         for (int j = 0; j < n; j++) { //n is columns
+//             cout<<arr_new [i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
 // }
-// int cost(int price, int quantity) {
-//     return price * quantity;
+
+// problem 6
+// int main() {
+//     int m1, n1, m2, n2;
+//     cout << "Enter rows and columns of Matrix A: ";
+//     cin >> m1 >> n1;
+//     cout << "Enter rows and columns of Matrix B: ";
+//     cin >> m2 >> n2;
+//     if (n1 != m2) {
+//         cout << "Matrix multiplication not possible. Columns of A must match rows of B." << endl;
+//         return 0;
+//     }
+//     int A[m1][n1], B[m2][n2], C[m1][n2];
+//     cout << "Enter elements of Matrix A: " << endl;
+//     for (int i = 0; i < m1; i++) {
+//         for (int j = 0; j < n1; j++) {
+//             cin >> A[i][j];
+//         }
+//     }
+//     cout << "Enter elements of Matrix B: " << endl;
+//     for (int i = 0; i < m2; i++) {
+//         for (int j = 0; j < n2; j++) {
+//             cin >> B[i][j];
+//         }
+//     }
+//     for (int i = 0; i < m1; i++) {
+//         for (int j = 0; j < n2; j++) {
+//             C[i][j] = 0;
+//         }
+//     }
+//     for (int i = 0; i < m1; i++) {
+//         for (int j = 0; j < n2; j++) {
+//             for (int k = 0; k < n1; k++) {  // or k < m2
+//                 C[i][j] += A[i][k] * B[k][j];
+//             }
+//         }
+//     }
+//     cout << "Resultant Matrix C:" << endl;
+//     for (int i = 0; i < m1; i++) {
+//         for (int j = 0; j < n2; j++) {
+//             cout << C[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+//problem 7
+// int main() {
+//     int a;
+//     cout<<"Enter the size: ";
+//     cin>>a;
+//
+//     int arr[a][a];
+//     cout<<"Elements: "<<endl;
+//     for (int i = 0; i < a; i++) {
+//         for (int j = 0; j < a; j++) {
+//             cin>>arr[i][j];
+//         }
+//     }
+//     for (int i = 0; i < a; i++) {
+//         for (int j = i+1; j < a; j++) {
+//             swap(arr[j][i], arr[i][j]);
+//         }
+//     }
+//     for (int i = 0; i < a; i++) {
+//         reverse (arr[i],arr[i]+a);
+//     }
+//     for (int i = 0; i < a; i++) {
+//         for (int j = 0; j < a; j++) {
+//             cout<<arr[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+//   strings
+
+//problem 1
+// int main() {
+//     string s;
+//     int vowels=0, constants = 0;
+//     getline(cin, s);
+//     for (int i=0; s.length()>i; i++) {
+//         char ch=s[i];
+//         if (ch>='a' && ch<='z') {
+//             if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+//                 vowels++;
+//             else
+//                 constants++;
+//         }
+//     }
+//     cout <<"vowels:"<< vowels<<endl;
+//     cout <<"constants:"<< constants;
+// }
+
+//problem 2
+// int main() {
+//    string s;
+//    getline(cin, s);
+//    reverse(s.begin(), s.end());
+//    cout<<s;
+//}
+
+//problem 3
+// int main() {
+//     string s, str1;
+//     getline(cin, s);
+//     str1=s;
+//     reverse(s.begin(), s.end());
+//     if (str1==s) {
+//         cout<<"yes";
+//     }else
+//         cout<<"no";
+//     }
+
+//problem 4
+// char mostFrequentchar(string s) {
+//     int count[256]={0};
+//     for(int i=0;i<s.length();i++)
+//         count[s[i]]++;
+//     int max=0;
+//     char mostChar;
+//     for(int i=0;i<256;i++)
+//         if(count[i]>max)
+//             mostChar=(char)i;
+//     return mostChar;
+//
 // }
 // int main() {
-//     int price=0, quantity=0, totalValue=0, fee=0, choice;
-//     char a, b, order;
-//     cout<<"From which city are you going?"; cin>>b;
-//     cout<<"Where are you going?"; cin>>a;
-//     do {
-//         train();
-//         cout<<"Enter a choice (1-3): "; cin>>choice;
-//         switch(choice) {
-//             case 1:
-//                 price=15;
-//             cout<<"Enter number of tickets: "; cin>>quantity;
-//             fee=cost(price, quantity);
-//             break;
-//             case 2:
-//                 price=20;
-//             cout<<"Enter number of tickets: "; cin>>quantity;
-//             fee=cost(price, quantity);
-//             break;
-//             case 3:
-//                 price=25;
-//             cout<<"Enter number of tickets: "; cin>>quantity;
-//             fee=cost(price, quantity);
-//             break;
-//             case 4:
-//                 cout<<"Exiting...";
-//             order = 'n';
-//             break;
-//             default:
-//                 cout<<"Enter valid choice"<<endl;
-//         }
-//             totalValue += fee;
-//             cout << "Running cost: $" << fee << endl;
-//         cout << "Total Value: $" << totalValue << endl;
-//             cout << "Do you want to buy more? (y/n): ";
-//             cin >> order;
-//     }while (order == 'y' || order == 'Y');
-//     cout<<"Total Value: $"<<totalValue<<endl;
+//     string s;
+//     char result;
+//     getline(cin, s);
+//     result=mostFrequentchar(s);
+//     if (result=='\0') {
+//        cout<<"empty";
+//     }else
+//         cout<<result;
 // }
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+
+//problem 5
+// string removeDuplicates(string s) {
+//     bool seen[256] = {false};
+//     string result = "";
+//     for (int i = 0; i < s.length(); i++) {
+//         if (!seen[s[i]]) {
+//             result += s[i];
+//             seen[s[i]] = true;
+//         }
+//     }
+//     return result;
+// }
+// int main() {
+//     string s;
+//     getline(cin, s);
+//     cout << removeDuplicates(s);
+// }
+
+//problem 6
+// int main() {
+//     string sentence;
+//     getline(cin, sentence);
+//     string word = "", longest = "";
+//     for (int i = 0; i < sentence.length(); i++){
+//         if (sentence[i] != ' ') {
+//             word +=sentence[i];
+//         } else {
+//             if (word.length() > longest.length()) {
+//                 longest = word;
+//             }
+//             word = "";
+//         }
+//     }
+//     if (word.length() > longest.length()) {
+//         longest = word;
+//     }
+//     cout << longest << endl;
+// }
+
+// problem 7
+// bool areAnagrams(string a, string b) {
+//     if (a.length() != b.length()) return false;
+//     sort(a.begin(), a.end());
+//     sort(b.begin(), b.end());
+//     return a == b;
+// }
+// int main() {
+//     string str1, str2;
+//     cin >> str1 >> str2;
+//     if (areAnagrams(str1, str2))
+//         cout << "Yes" << endl;
+//     else
+//         cout << "No" << endl;
+// }
